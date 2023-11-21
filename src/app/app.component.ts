43fb66f2f9c61ae109d562bcd214dba1cec3a48a
Lucky Dogbey <luckydogbey@gmail.com>
2023-11-21 11:43:33 +0000

    finished life cycle class

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnChanges, OnInit, SimpleChanges } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { LoginComponent } from './auth/login/login.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, FormsModule, LoginComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
+export class AppComponent implements OnChanges, OnInit  {
+
   title = 'inmest-web';
+  name = "Lucky";
+  profile = {
+    id: 1,
+    first_name: "Lucky",
+    last_name: "Dogbey"
+  }
+  genesis = "hello";
+
+  ngOnInit(): void {
+    console.log('On Init');
+  }
+  constructor() {
+    console.log("constructor");
+  }
+  ngOnChanges(changes: SimpleChanges): void {
+    console.log(changes, 'changes');
+    for(const inputChange in changes) {
+      console.log(changes[inputChange].firstChange, inputChange);
+    }
+  }
+
+  
 }
