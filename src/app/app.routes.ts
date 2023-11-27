3a3f4805f90dcbfa7cc010e5dd048ae3354fdc16
Lucky Dogbey <luckydogbey@gmail.com>
2023-11-27 13:29:06 +0000

    working on url params

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,15 @@ import { Routes } from '@angular/router';
 import { DashboardComponent } from './main-features/dashboard/dashboard.component';
 import { UsersComponent } from './main-features/users/users.component';
 import { UserDetailComponent } from './main-features/user-detail/user-detail.component';
+import { MyRequestsComponent } from './main-features/my-requests/my-requests.component';
+import { MyClassesComponent } from './main-features/my-classes/my-classes.component';
+import { LeavesComponent } from './main-features/leaves/leaves.component';
 
 export const routes: Routes = [
     {path: '', component: DashboardComponent},
     {path: 'users', component: UsersComponent},
+    {path: 'my-requests', component: MyRequestsComponent},
+    {path: 'my-classes', component: MyClassesComponent},
+    {path: 'leaves', component: LeavesComponent},
     {path: 'user-detail', component: UserDetailComponent}
 ];
---
+++ src/app/main-features/leaves/leaves.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-leaves',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './leaves.component.html',
+  styleUrl: './leaves.component.scss'
+})
+export class LeavesComponent {
+
+}
